b24aa4134e4f335145eb490bf6f11c7cc3e19a15
marmeladka0075 <163419153+marmeladka0075@users.noreply.github.com>
2026-06-22 23:09:56 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -39,41 +39,54 @@ class GameController:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.process_action()
-
                 elif event.key == pygame.K_ESCAPE:
                     if self.model.game_state == "GAME_OVER":
                         self.model.reset_game()
                         self.model.game_state = "MENU"
-                    elif self.model.game_state == "SETTINGS":
+                    elif self.model.game_state in ["SETTINGS", "SHOP"]:
                         self.model.game_state = "MENU"
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mx, my = pygame.mouse.get_pos()
+                    state = self.model.game_state
 
-                    if self.model.game_state == "MENU":
-                        if 300 <= mx <= 500 and 250 <= my <= 300:
+                    if state == "MENU":
+                        if 300 <= mx <= 500 and 240 <= my <= 290:
                             self.model.game_state = "PLAYING"
-                        elif 300 <= mx <= 500 and 330 <= my <= 380:
+                        elif 300 <= mx <= 500 and 310 <= my <= 360:
+                            self.model.game_state = "SHOP"
+                        elif 300 <= mx <= 500 and 380 <= my <= 430:
                             self.model.game_state = "SETTINGS"
 
-                    elif self.model.game_state == "SETTINGS":
-                        # Клик по кнопке "-"
+                    elif state == "SETTINGS":
                         if 190 <= mx <= 230 and 245 <= my <= 285:
                             self.model.volume = max(0.0, self.model.volume - 0.1)
-                            self.view.update_volume()  # Применяем громкость к музыке
-                            self.view.play_sound('jump')  # Пикаем для проверки громкости
-
-                        # Клик по кнопке "+"
+                            self.view.update_volume()
+                            self.view.play_sound('jump')
                         elif 570 <= mx <= 610 and 245 <= my <= 285:
                             self.model.volume = min(1.0, self.model.volume + 0.1)
                             self.view.update_volume()
                             self.view.play_sound('jump')
-
                         elif 300 <= mx <= 500 and 450 <= my <= 500:
                             self.model.game_state = "MENU"
+                            self.model.save_progress()
 
-                    elif self.model.game_state in ["PLAYING", "GAME_OVER"]:
+                    elif state == "SHOP":
+                        if 300 <= mx <= 500 and 500 <= my <= 550:  # BACK
+                            self.model.game_state = "MENU"
+                            return
+
+                        y_offset = 180
+                        for skin in self.model.skins_catalog.keys():
+                            if 500 <= mx <= 630 and (y_offset + 10) <= my <= (y_offset + 40):
+                                if self.model.buy_or_equip_skin(skin):
+                                    self.view.play_sound('buff')
+                                else:
+                                    self.view.play_sound('crash')
+                            y_offset += 60
+
+                    elif state in ["PLAYING", "GAME_OVER"]:
                         self.process_action()
 
     def process_action(self):
